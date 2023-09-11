08ded52190bf8f4810507d07ee986bd22d7fe89f
Vraj Kavathiya <vrajk.albiorix@gmail.com>
2023-09-11 19:29:20 +0530

    Added Form input for child, Remove and add functionality in parent, Design improvement, Code Refactor and Added comments for functions

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Collection } from '../models/collection.model';
+import { Collection, collectionType } from '../models/collection.model';
 
 @Component({
   selector: 'app-collection',
@@ -8,21 +8,53 @@ import { Collection } from '../models/collection.model';
 })
 export class CollectionComponent {
   @Input() collectionData: Collection[] = [];
-  isAdd = false;
-  isAddClicked = false;
+  @Input() parent: Collection | null = null; // parent===null represents "root" folder
 
-  // adds child folder or file; functionality not completed; currently only adds file-folder option
-  onAddChild(item: Collection): void {
-    item.children?.push({
-      type: 'Folder',
-      name: 'Folder Children 1.1',
-      children: [],
-      isShowFolderFileOption: true,
-    });
+  // sets item's select type and trigger it on UI
+  triggerInput(item: Collection, type: collectionType) {
+    item.selectionType = type;
   }
 
-  // removes particular item from children array; functionality not completed; currently removes child instead of that particular item
-  onRemove(item: Collection, i: number): void {
-    item.children?.splice(i);
+  // after user submits the form input(separate component), set the added item's property
+  onUserSubmittedForm(
+    name: string,
+    parent: Collection | null,
+    item: Collection
+  ) {
+    item.showOptions = false;
+    item.name = name;
+    item.selectionType && (item.type = item.selectionType);
+    parent && (parent.canAddChild = true); // reset canAddChild of it's parent to let parent add another item
+  }
+
+  // after user cancels the form input(separate component), discard the pushed dummy child from parent and reset isChild for letting user select File/Folder
+  onFormCancelled(parent: Collection | null) {
+    parent?.children?.pop(); //
+    parent && (parent.canAddChild = true);
+  }
+
+  // if one dummy child is already added, then no need to add another just return; else; add
+  onAddChild(item: Collection): void {
+    if (!item.canAddChild) {
+      return;
+    }
+    // else; push dummy child for showing file-folder select option and later apply necessary changes through "onUserSubmittedForm()"
+    item.children?.push({
+      type: 'Folder',
+      name: 'Dummy',
+      children: [],
+      showOptions: true,
+      selectionType: null,
+      canAddChild: true,
+    });
+    item.canAddChild = true;
+  }
+
+  // removes particular item from parent array
+  onRemove(index: number): void {
+    // if element to be removed is "root" folder, then remove it from whole data i.e. collectionData, else; from its "parent"
+    this.parent === null
+      ? this.collectionData.splice(index, 1)
+      : this.parent?.children?.splice(index, 1);
   }
 }
